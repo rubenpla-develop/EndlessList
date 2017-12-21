f734ba2ccec6915f76bc81cc30ceabdcef01e232
Ruben Pla <ruben.pla@alten.es>
2017-12-21 10:45:56 +0100

    [BINDING/MVP] activity_main.xml Bindging actions in collaboration with MainActivityPresenter class

--- app/src/main/java/rubenpla/develop/endlesslist/databinding/BindingAdapter.kt
+++ app/src/main/java/rubenpla/develop/endlesslist/databinding/BindingAdapter.kt
@@ -3,16 +3,24 @@ package rubenpla.develop.endlesslist.databinding
 import android.databinding.BindingAdapter
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.RecyclerView
+import rubenpla.develop.endlesslist.listener.RecyclerViewScrollCallback
 
 /**
  * Created by alten on 20/12/17.
  */
 class BindingAdapter {
 
-    @BindingAdapter(value = *arrayOf("visibleThreshold", "resetLoading"), requireAll = true)
+    @BindingAdapter(value = *arrayOf("visibleThreshold", "resetLoading", "onScrollToBottom"), requireAll = false)
     fun setRecyclerViewScrollCallback(recyclerView: RecyclerView, visibleThreshold: Int,
-                                      resetLoading : Boolean) {
+                                      resetLoading : Boolean, onScrolledListener: RecyclerViewScrollCallback.OnScrolledListener) {
+        val callback = RecyclerViewScrollCallback.Builder(recyclerView.layoutManager)
+                .resetLoadingState(resetLoading)
+                .visibleThreshold(visibleThreshold)
+                .onScrolledListener(onScrolledListener)
+                .build()
 
+        recyclerView.clearOnScrollListeners()
+        recyclerView.addOnScrollListener(callback)
     }
 
     @BindingAdapter(value = ["onPullSwipeRefresh"])
